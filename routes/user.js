018674b5ca05522fc450705efebc757552cba255
RashmiKhanagwal <rashmi.khanagwal@gmail.com>
2022-03-01 17:45:21 +0530

    user routes file

---
+++ routes/user.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const User = require("../controllers/user");
+const Admin = require("../middleware/admin")
+
+router.get('/showallUser', Admin, User.getAllTodo)
+router.post('/register', User.createUser)
+router.post('/login', User.login)
+router.get('/showUser/:id', Admin, User.getUser)
+router.patch('/updateUser/:id', Admin, User.updateUser)
+router.delete('/deleteallUser', Admin, User.deleteAllUser)
+router.delete('/deleteUser/:id', Admin, User.deleteUser)
+
+// Day 5
+router.get('/todayRegistered', User.todayRegistered)
+router.get('/todayActive', User.todayActive)
+router.get('/weekActive', User.weekActive)
+router.get('/monthActive', User.monthActive)
+
+module.exports = router;
